0d61b3360dd9903e9c842c1dc9e9a83985ee6ea7
PaulSidor20000 <pavel.sidoroff2014@yandex.ru>
2023-03-12 19:45:57 +0300

    feature: add UserController

--- src/main/java/ru/practicum/shareit/user/UserController.java
+++ src/main/java/ru/practicum/shareit/user/UserController.java
@@ -1,13 +1,35 @@
 package ru.practicum.shareit.user;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * TODO Sprint add-controllers.
  */
 @RestController
+@RequiredArgsConstructor
 @RequestMapping(path = "/users")
 public class UserController {
+    private final UserService userService;
+
+    @PostMapping
+    public User create(@RequestBody User user) {
+        return userService.create(user);
+    }
+
+    @GetMapping
+    public User read(@RequestBody Long userId) {
+        return userService.read(userId);
+    }
+
+    @PatchMapping
+    public User update(@RequestBody User user) {
+        return userService.update(user);
+    }
+
+    @DeleteMapping
+    public User delete(@RequestBody Long userId) {
+        return userService.delete(userId);
+    }
 
 }
--- src/main/java/ru/practicum/shareit/user/UserService.java
+++ src/main/java/ru/practicum/shareit/user/UserService.java
@@ -1,4 +1,8 @@
 package ru.practicum.shareit.user;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+
 }
